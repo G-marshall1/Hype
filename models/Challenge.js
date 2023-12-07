397df86980601de501d8c49a10b8d858ee35ea6a
Kwinn-Korth <kjkorth12@gmail.com>
2023-12-06 19:24:05 -0700

    challenge export

--- models/Challenge.js
+++ models/Challenge.js
@@ -1,39 +1,37 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
-module.exports = (sequelize) => {
-  const Challenge = sequelize.define('Challenge', {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 255],
-      },
+const Challenge = sequelize.define('Challenge', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 255],
     },
-    description: {
-      type: DataTypes.TEXT,
-      defaultValue: '',
+  },
+  description: {
+    type: DataTypes.TEXT,
+    defaultValue: '',
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+  indexes: [
+    {
+      unique: true,
+      fields: ['startDate'],
     },
-    startDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
+    {
+      fields: ['endDate'],
     },
-    endDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-  }, {
-    timestamps: true,
-    indexes: [
-      {
-        unique: true,
-        fields: ['startDate'],
-      },
-      {
-        fields: ['endDate'],
-      },
-    ],
-  });
+  ],
+});
 
-  return Challenge;
-};
+module.exports = Challenge;
