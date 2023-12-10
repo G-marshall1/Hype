745c41a9031679dd67e43280300fa3c6157e52f8
Kwinn-Korth <kjkorth12@gmail.com>
2023-12-09 21:12:19 -0700

    seperated home and login routes

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { VideoPost, User } = require('../models');
-const withAuth = require('../utils/auth');
+const { VideoPost, User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -48,37 +48,6 @@ router.get('/videopost/:id', async (req, res) => {
     console.log(err)
     res.status(500).json(err);
   }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: VideoPost }],
-    });
-
-    const user = userData.get({ plain: true });
-    console.log(user)
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
 });
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const videoPostRoutes = require('./videoPostRoutes');
 const commentRoutes = require('./commentRoutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/videoPosts', videoPostRoutes);
 router.use('/comments', commentRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
